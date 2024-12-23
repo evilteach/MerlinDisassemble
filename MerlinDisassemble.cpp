472f01d5b8f49c08f7da940fd5897b2836caddd1
jslater@home <ka3bzq@gmail.com>
2024-12-23 16:30:02 -0500

    Porting to code blocks

--- MerlinDisassemble.cpp
+++ MerlinDisassemble.cpp
@@ -1,27 +1,27 @@
-//                                                                                  
-//  MerlinDisassemble.cpp                                                           
-//                                                                                  
-// MIT License                                                                      
-//                                                                                  
-// Copyright (c) 2024 EvilTeach                                                     
-//                                                                                  
-// Permission is hereby granted, free of charge, to any person obtaining a copy     
-// of this software and associated documentation files (the "Software"), to deal    
-// in the Software without restriction, including without limitation the rights     
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell        
-// copies of the Software, and to permit persons to whom the Software is            
-// furnished to do so, subject to the following conditions:                         
-//                                                                                  
-// The above copyright notice and this permission notice shall be included in all   
-// copies or substantial portions of the Software.                                  
-//                                                                                  
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR       
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,         
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE      
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER           
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,    
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    
-// SOFTWARE.                                                                        
+//
+//  MerlinDisassemble.cpp
+//
+// MIT License
+//
+// Copyright (c) 2024 EvilTeach
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #include <iostream>
 #include <iomanip>
@@ -60,110 +60,117 @@ int main
 
     MerlinDisassemble merlinDisassemble;
 
-    try
+    if (argC > 1)
     {
-        int firstArgument = 1;
 
-        std::size_t z = 1;
-        for (; z <= (std::size_t) argC; z++)
+        try
         {
-            if (argV[z][0] == '-')
-            {
-                firstArgument = (int) z + 1;
-                std::string argument = argV[z];
-                trim(argument);
-                uppercase(argument);
+            int firstArgument = 1;
 
-                if (argument.substr(0, 2) == "-L")
+            std::size_t z = 1;
+            for (; z <= (std::size_t) argC; z++)
+            {
+                if (argV[z][0] == '-')
                 {
-                    merlinDisassemble.show_line_numbers();
-                }
-                else
-                {
-                    if (argument.substr(0, 3) == "-C:")
+                    firstArgument = (int) z + 1;
+                    std::string argument = argV[z];
+                    trim(argument);
+                    uppercase(argument);
+
+                    if (argument.substr(0, 2) == "-L")
                     {
-                        merlinDisassemble.set_comment_column(std::stoi(argument.substr(3)));
+                        merlinDisassemble.show_line_numbers();
                     }
                     else
                     {
-                        if (argument.substr(0, 2) == "-H")
+                        if (argument.substr(0, 3) == "-C:")
                         {
-                            std::cerr << helpText << std::endl;
-                            throw 1;
+                            merlinDisassemble.set_comment_column(std::stoi(argument.substr(3)));
                         }
                         else
                         {
-                            std::cerr << "Argument <" << argument
-                                      << "> is not recognized." << std::endl;
-                            throw 1;
+                            if (argument.substr(0, 2) == "-H")
+                            {
+                                std::cerr << helpText << std::endl;
+                                throw 1;
+                            }
+                            else
+                            {
+                                std::cerr << "Argument <" << argument
+                                          << "> is not recognized." << std::endl;
+                                throw 1;
+                            }
                         }
                     }
                 }
+                else
+                {
+                    // Command line switches must be on the front.
+                    // In this case there are not any more.
+
+                    break;
+                }
             }
-            else
+
+            int argumentCount = argC - firstArgument;
+            if (argumentCount == 0)
             {
-                // Command line switches must be on the front.
-                // In this case there aren't any more.
+                // MerlinDisassemble
 
-                break;
-            }
-        }
-
-
-        int argumentCount = argC - firstArgument;
-        if (argumentCount == 0)
-        {
-            // MerlinDisassemble
-
-            std::istream &in = std::cin;
-            std::ostream &out = std::cout;
-            returnStatus = merlinDisassemble.process(in, out);
-        }
-        else
-        {
-            if (argumentCount == 1)
-            {
-                // MerlinDisassemble input.txt
-
-                std::ifstream in (argV[firstArgument + 0]);
+                std::istream &in = std::cin;
                 std::ostream &out = std::cout;
-
                 returnStatus = merlinDisassemble.process(in, out);
             }
             else
             {
-                if (argumentCount == 2)
+                if (argumentCount == 1)
                 {
-                    // MerlinDisassemble input.txt output.txt
+                    // MerlinDisassemble input.txt
 
-                    std::ifstream in (argV[firstArgument  + 0]);
-                    std::ofstream out (argV[firstArgument + 1], std::ofstream::out);
+                    std::ifstream in (argV[firstArgument + 0]);
+                    std::ostream &out = std::cout;
+
                     returnStatus = merlinDisassemble.process(in, out);
                 }
                 else
                 {
-                    std::cerr << "argumentCount is " << argumentCount << std::endl;
-                    std::cerr << "Something must be odd about the command line" << std::endl;
-
-                    for (int y = 0; y < argC; y++)
+                    if (argumentCount == 2)
                     {
-                        std::cerr << y << " <" << argV[y] << ">" << std::endl;
-                    }
+                        // MerlinDisassemble input.txt output.txt
 
-                    throw(1);
+                        std::ifstream in (argV[firstArgument  + 0]);
+                        std::ofstream out (argV[firstArgument + 1], std::ofstream::out);
+                        returnStatus = merlinDisassemble.process(in, out);
+                    }
+                    else
+                    {
+                        std::cerr << "argumentCount is " << argumentCount << std::endl;
+                        std::cerr << "Something must be odd about the command line" << std::endl;
+
+                        for (int y = 0; y < argC; y++)
+                        {
+                            std::cerr << y << " <" << argV[y] << ">" << std::endl;
+                        }
+
+                        throw(1);
+                    }
                 }
             }
         }
+        catch (const std::exception &e)
+        {
+            std::cerr << "Exception " << e.what() << std::endl;
+            returnStatus = 1;
+        }
+        catch (...)
+        {
+            std::cerr << "Unexpected Exception " << std::endl;
+            returnStatus = 1;
+        }
     }
-    catch (const std::exception &e)
+    else
     {
-        std::cerr << "Exception " << e.what() << std::endl;
-        returnStatus = 1;
-    }
-    catch (...)
-    {
-        std::cerr << "Unexpected Exception " << std::endl;
-        returnStatus = 1;
+        std::cerr << "Provide at least one argument." << std::endl;
     }
 
     return returnStatus;
